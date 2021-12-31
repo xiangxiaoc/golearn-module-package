635a9047f7115225cd029872f0c7e84c548b7d9d
Oranx <xiangxiaoc@vip.qq.com>
2021-12-31 21:00:17 +0800

    增加错误处理

--- fat_rate_calculator.go
+++ fat_rate_calculator.go
@@ -4,16 +4,22 @@ import (
 	"fmt"
 	gobmi "github.com/armstrongli/go-bmi"
 	"github.com/xiangxiaoc/golearn-module-package/type_in"
+	"os"
 )
 
 func main() {
 	name, sex, age, height, weight := type_in.TypeIN()
 	bmi, err := gobmi.BMI(weight, height)
 	if err != nil {
-		return
+		fmt.Println("ERROR: ", err)
+		os.Exit(1)
 	}
 
-	fatRate := gobmi.CalFatRate(sex, bmi, age)
+	fatRate, err2 := gobmi.CalFatRate(sex, bmi, age)
+	if err2 != nil {
+		fmt.Println("ERROR: ", err2)
+		os.Exit(1)
+	}
 
 	fmt.Println("======= 输出结果 =======")
 	fmt.Printf("姓名：%s BMI：%.2f 体脂率：%.2f", name, bmi, fatRate)
